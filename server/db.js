9bf5a18b4e8be3848c68206435262f7711b5a289
sal garcia <salgarcia930@gmail.com>
2022-11-28 22:42:31 +0000

    fix typo in pg pool args

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL, // comming from the .env file
   ssl: {
-    rejectUnathorized: false
+    rejectUnauthorized: false
   }
 });
 
